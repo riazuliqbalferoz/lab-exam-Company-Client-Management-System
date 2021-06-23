2d68de8ead9e4ea1d00c09e65739ecfd18ee965d
Riaz <riazuliqbalferz@gmail.com>
2021-06-23 18:01:47 +0600

    all user registration done

--- app/Http/Controllers/customer_controller.php
+++ app/Http/Controllers/customer_controller.php
@@ -3,8 +3,43 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\DB;
 class customer_controller extends Controller
 {
-    //
+
+    function onInsert(Request $req){
+    
+        return view('add_user');
+    }    
+function add(Request $request){
+
+ $request ->validate([
+'name'=>'required',
+'address'=>'required',
+'email'=>'required',
+'passwrod'=>'required',
+
+
+        ]);
+
+
+$query = DB::table ('customer')->insert([
+
+
+'Name'=>$request ->input('name'),
+'address'=>$request ->input('address'),
+'email'=>$request ->input('email'),
+'password'=>$request ->input('passwrod'),
+
+
+
+]);
+if ($query){
+   return back()->with('success','data has been inserted Successfully');     
+}
+
+else{
+        return back()->with('filed');        
+}
+}
 }
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 use App\Http\Controllers\loginController;
-
-
+use App\Http\Controllers\customer_controller;
+use App\Http\Controllers\admin_controller;
+use App\Http\Controllers\account_controller;
+use App\Http\Controllers\sales_controller;
 
 Route::get('/login', [loginController::class, 'login']);
 Route::get('/admin_profile', [loginController::class, 'onSelect']);
 
+
+//route for custormer data insert
+Route::get('/add', [customer_controller::class, 'add']);
+Route::get('/add_user', [customer_controller::class, 'onInsert']);
+
+//route for admin data insert
+Route::get('/add', [admin_controller::class, 'add']);
+Route::get('/add_accountent', [admin_controller::class, 'onInsert']);
+
+//route for accountan data insert
+Route::get('/add', [account_controller::class, 'add']);
+Route::get('/add_salesman', [account_controller::class, 'onInsert']);
+
+//route for salesman data insert
+Route::get('/add', [sales_controller::class, 'add']);
+Route::get('/add_admin', [sales_controller::class, 'onInsert']);
