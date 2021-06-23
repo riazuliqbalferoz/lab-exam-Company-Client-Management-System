82724a5895c694ceb2879b4e36cf1b796fe24e87
Riaz <riazuliqbalferz@gmail.com>
2021-06-23 11:42:34 +0600

    project created for clint management syste

---
+++ resources/views/login.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>All User Login </title>
+</head>
+<body>
+	<form method="post">
+	<table>
+		<!-- @csrf -->
+		<!-- {{csrf_field()}} -->
+		<input type="hidden" name="_token" value="{{csrf_token()}}">
+		<tr>
+			<td>Name</td>
+			<td><input type="text" name="uname" value="{{old('uname')}}"></td>
+		</tr>
+		<tr>
+			<td>Password</td>
+			<td><input type="password" name="password" value="{{old('password')}}"></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td><input type="submit" name="Submit" value="submit"></td>
+		</tr>
+	</table>
+	</form>
+		{{session('msg')}}
+		<br>
+
+		@foreach ($errors->all() as $err)
+			{{$err}} <br>
+		@endforeach
+</body>
+</html>
+
